5b599dcce3589004baa249eba3a9810583bd87f6
Irfan Gumelar <irfangumelar@gmail.com>
2021-02-24 08:58:54 +0700

    apply secondary color on subtitle

--- src/layouts/shared/ContentContainer/ContentContainer.js
+++ src/layouts/shared/ContentContainer/ContentContainer.js
@@ -4,8 +4,40 @@ const Wrapper = styled.div`
   padding: 2rem;
 `;
 
+const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+    margin-bottom:2rem;
+`
+
+const Title = styled.h1`
+    font-weight: semibold;
+    font-size: 2.5rem;
+`
+
+const SubtTitle = styled.h3`
+    margin-top: .5rem;
+    font-weight: 400;
+    font-size: 1.3rem;
+    color: ${props => props.theme.textColorSecondary};
+`
+
 const ContentContainer = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+        <TitleContainer>
+            <Title className="p-reset">
+                Create Products
+            </Title>
+
+            <SubtTitle className="p-reset">
+                Add new product and set the categoriess
+            </SubtTitle>
+        </TitleContainer>
+        {children}
+    </Wrapper>
+  );
 };
 
 export default ContentContainer;
